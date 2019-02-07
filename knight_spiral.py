507cd0a2d57eb3a67aea15fd95b4fc2c3c9cbe06
johanng1 <johann.ubuntu@gmail.com>
2019-02-07 12:46:54 +0000

    have got a function to make a spiral board working - with positions of each number (starting at 1 : (0, 0)

--- knight_spiral.py
+++ knight_spiral.py
@@ -6,14 +6,12 @@ class Spiral_Board:
 
 	#def __init__(self):
 
-	spiral_positions = {
-	(0,0): 1,
-	}
 
 	def calculate_spiral_number(self, position):
-		if position in self.spiral_positions:
-			return spiral_positions[position]
+		if position in self.already_visited:
+			return "already visited"
 		else:
+			already_visited.append(position)
 			pass # this is the hard bit
 			# need to work out how to calculate the number based on the 2d coordinates
 
@@ -26,3 +24,69 @@ class Spiral_Board:
 		return pos_moves
 
 
+	def move_right(self, pos):
+		(x, y) = pos
+		return (x+1, y)
+
+	def move_up(self, pos):
+		(x, y) = pos
+		return (x, y+1)
+
+	def move_left(self, pos):
+		(x, y) = pos
+		return (x-1, y)
+
+	def move_down(self, pos):
+		(x, y) = pos
+		return (x, y-1)
+
+
+
+
+	def make_spiral_board(self, num_range):
+		spiral_positions = {
+		(0, 0): 1
+		}
+
+		moves = ["right", "up", "left", "down"]
+		position = (0, 0)
+		move_until = 1
+		counter_move_twice = 1
+		counter_until = 1
+		for i in range(2, num_range):
+
+			if moves[0] == "right":
+				position = self.move_right(position)
+			if moves[0] == "up":
+				position = self.move_up(position)
+			if moves[0] == "left":
+				position = self.move_left(position)
+			if moves[0] == "down":			
+				position = self.move_down(position)
+
+			
+
+			spiral_positions[position] = i
+
+
+			if counter_until == move_until:
+				counter_until = 1
+				moves = moves[1:] + [moves[0]]
+				if counter_move_twice == 2:
+					counter_move_twice = 1
+					move_until += 1
+				else:
+					counter_move_twice += 1
+				
+			else:
+				counter_until += 1
+
+
+		return spiral_positions
+
+
+
+
+
+
+print(Spiral_Board().make_spiral_board(10))
